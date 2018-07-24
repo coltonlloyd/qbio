4c769e4c41267395eec488b9d9b70474e39ca2fb
coltonlloyd <cjlloyd@eng.ucsd.edu>
2018-07-24 16:58:22 -0700

    Reorganize

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+
+setup(name="qbio18",
+      version="",
+      author="Colton Lloyd",
+      url="https://github.com/coltonlloyd/qbio",
+      packages=find_packages()
+      )
